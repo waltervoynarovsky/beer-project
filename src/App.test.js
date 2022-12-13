22f6c68654af9ce082328943d24b01c64f805f25
Walter Wojnarowski <walterwojnarowski@hotmail.com>
2022-12-13 16:03:49 +0000

    delicate styling and 3 basic tests added

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,24 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App tests", () => {
+  it("should render the search bar", () => {
+    render(<App />);
+    const searchBar = screen.getByText("Search beers:");
+    expect(searchBar).toBeInTheDocument();
+  });
+
+  it("should render the filters", () => {
+    render(<App />);
+    const filters = screen.getByText(
+      "High abv (> 6.0%)" && "Classic range" && "Acidic(ph < 4)"
+    );
+    expect(filters).toBeInTheDocument;
+  });
+
+  it("should render the beers", () => {
+    render(<App />);
+    const beers = screen.queryByTestId("beers");
+    expect(beers).toBeInTheDocument;
+  });
 });
