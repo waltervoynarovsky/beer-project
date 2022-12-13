d2eae7f656fc168e937f4c4516b4b6b4f3fb92dc
Walter Wojnarowski <walterwojnarowski@hotmail.com>
2022-12-13 14:38:18 +0000

    Acidity working and styling improved

--- src/App.jsx
+++ src/App.jsx
@@ -7,42 +7,52 @@ function App() {
   const [searchTerm, setSearchTerm] = useState("");
   const [filter, setFilter] = useState("");
   const [beers, setBeers] = useState([]);
-
+  // const [acidity, setAcidity] = useState(false);
   const getBeers = async () => {
     let url = "";
 
     if (filter === "high abv (> 6.0%)") {
       url = `https://api.punkapi.com/v2/beers/?abv_gt=6`;
     } else if (filter === "classic range") {
-      url = `https://api.punkapi.com/v2/beers/?brewed_before=12-2010`;
-    } else if (filter === "acidic(ph < 4)") {
-      const acidity = beers.ph < 4;
-      return acidity;
+      url = `https://api.punkapi.com/v2/beers/?brewed_before=01-2010`;
     } else {
       url = `https://api.punkapi.com/v2/beers`;
     }
     const result = await fetch(url);
     const beerData = await result.json();
     setBeers(beerData);
+    // const beerLowerCase = beerData.name.toLowerCase();
+    // return beerLowerCase.includes(searchTerm);
 
-    const beerLowerCase = beerData.name.toLowerCase();
-    return beerLowerCase.includes(searchTerm);
+    if (filter === "acidic(ph < 4)") {
+      const highAcidity = beerData.filter((beer) => {
+        const acidity = beer.ph < 4 && beer.ph !== null;
+        return acidity;
+      });
+      setBeers(highAcidity);
+    } else {
+      setBeers(beerData);
+    }
   };
+
   const handleOnChange = (event) => {
     setFilter(event.target.value);
   };
-
-  useEffect(() => {
-    getBeers();
-  }, [filter]);
 
   const handleInput = (event) => {
     const cleanInput = event.target.value.toLowerCase();
     setSearchTerm(cleanInput);
     console.log(handleInput);
   };
-  // store whether checkbox is clicked or not
-  //
+
+  const filteredBeers = beers.filter((search) => {
+    const beersLower = search.name.toLowerCase();
+    return beersLower.includes(searchTerm);
+  });
+
+  useEffect(() => {
+    getBeers();
+  }, [filter]);
 
   return (
     <div className="app">
@@ -52,7 +62,7 @@ function App() {
         handleOnChange={handleOnChange}
         selected={filter}
       />
-      <CardList beerArr={beers} />
+      <CardList beerArr={filteredBeers} />
     </div>
   );
 }
